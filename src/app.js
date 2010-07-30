bb6d8a25bbb34e2e844db07d755aaa8534dbd9e3
Brian Mavity <brian@brianmavity.com>
2010-07-30 14:39:21 -0500

    Updated app.js to express 1.0RC

--- src/app.js
+++ src/app.js
@@ -3,29 +3,25 @@ var sys = require('sys'),
 	app = require('express').createServer(),
 	pub = __dirname + '/public';
 
-app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	
-	app.use('/', connect.logger());
-	app.use('/', connect.bodyDecoder());
-	app.use('/', connect.methodOverride());
-	app.use('/', connect.compiler({ src: pub, enable: ['sass'] }));
-	app.use('/', connect.staticProvider(pub));
-	
-	sys.puts('global configure');
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
+app.use(connect.logger());
+app.use(connect.bodyDecoder());
+app.use(connect.methodOverride());
+app.use(connect.compiler({ src: pub, enable: ['sass'] }));
+app.use(connect.staticProvider(pub));
+	
 app.configure('development', function() {
 	app.set('reload views', 1000);
 	
-	app.use('/', connect.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
 	
 	sys.puts('dev configure');
 });
 
 app.configure('production', function() {
-	app.use('/', connect.errorHandler());
+	app.use(connect.errorHandler());
 	
 	sys.puts('prod configure');
 });
