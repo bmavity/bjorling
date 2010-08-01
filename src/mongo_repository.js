09dc7606fb98568447bbaa8a11651290d6fabf3f
Brian Mavity <brian@brianmavity.com>
2010-08-01 14:26:27 -0500

    fixed misunderstanding of find returning a cursor instead of results and formatted tabs in mongo repository

--- src/mongo_repository.js
+++ src/mongo_repository.js
@@ -1,27 +1,29 @@
 var sys = require('sys'),
     mongo = require('mongodb');
 
-var db = new mongo.Db('node-mongo-blog', new mongo.Server('localhost', 27017, {}, {}));
+var db = new mongo.Db('node-mongo-blog', new mongo.Server('localhost', 27017, { auto_reconnect: true }, {}));
 
 var session = function(callback) {
-    db.open(function(err, repo) {
-        repo.collection('posts', function(err, coll) {
-            callback(coll);
-            db.close();
-        });
+  db.open(function(err, repo) {
+    repo.collection('posts', function(err, coll) {
+      callback(coll);
+      db.close();
     });
+  });
 };
 
 exports.findAll = function(callback) {
-    session(function(collection) {
-        collection.find(function(err, results) {
-            callback(err, results);
-        });
+  session(function(collection) {
+    collection.find(function(err, cursor) {
+      cursor.toArray(function(err, results) {
+        callback(err, results);
+      });
     });
+  });
 };
 
 exports.save = function(post) {
-    session(function(collection) {
-        collection.insert(post);
-    });
+  session(function(collection) {
+    collection.insert(post);
+  });
 };
