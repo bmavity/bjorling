83509033d03a37991d849d320d208408f290329c
Brian Mavity <brian@brianmavity.com>
2010-08-01 11:36:56 -0500

    changed remaining tabs into spaces

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 var sys = require('sys'),
-	connect = require('connect'),
-	app = require('express').createServer(),
+    connect = require('connect'),
+    app = require('express').createServer(),
     repo = require('./mongo_repository'),
-	pub = __dirname + '/public';
+    pub = __dirname + '/public';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -12,19 +12,19 @@ app.use(connect.bodyDecoder());
 app.use(connect.methodOverride());
 app.use(connect.compiler({ src: pub, enable: ['sass'] }));
 app.use(connect.staticProvider(pub));
-	
+    
 app.configure('development', function() {
-	app.set('reload views', 1000);
-	
-	app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
-	
-	sys.puts('dev configure');
+    app.set('reload views', 1000);
+    
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
+    
+    sys.puts('dev configure');
 });
 
 app.configure('production', function() {
-	app.use(connect.errorHandler());
-	
-	sys.puts('prod configure');
+    app.use(connect.errorHandler());
+    
+    sys.puts('prod configure');
 });
 
 
