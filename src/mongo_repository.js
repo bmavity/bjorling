2af9ec105f60fadd2923d3b597eb98043cb2c6d6
Brian Mavity <brian@brianmavity.com>
2010-08-03 15:26:56 -0500

    added method to integrate slugs into sample date

--- src/mongo_repository.js
+++ src/mongo_repository.js
@@ -56,3 +56,19 @@ exports.updatePublishDate = function() {
     });
   });
 };
+
+exports.updateSlugs = function() {
+  session(function(collection) {
+    collection.find(function(err, cursor) {
+      cursor.toArray(function(err, posts) {
+        require('fs').readFile(__dirname + '/../data/slugs.json', function(err, lines) {
+          var slugLines = lines.toString().split('\r\n');
+          for(var i = 0; i < posts.length; i += 1) {
+            posts[i].slug = slugLines[i].split(' ')[0];
+            collection.update({ _id: posts[i]._id }, posts[i]);
+          }
+        });
+      });
+    });
+  });
+};
