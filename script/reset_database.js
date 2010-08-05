7b87fa085b1275a443756b44780254cdd250f2df
Brian Mavity <brian@brianmavity.com>
2010-08-05 16:29:10 -0500

    Separated files for initial import from current files

--- script/reset_database.js
+++ script/reset_database.js
@@ -4,23 +4,13 @@ var sys = require('sys'),
     scriptDir = __dirname,
     dataDir = scriptDir + '/../data/';
 
-sys.puts('Loading data from: ' + dataDir);
-
-var camelize = function(obj) {
-  var cameled = {};
-  for(var key in obj) {
-    cameled[key.charAt(0).toLowerCase() + key.substring(1)] = obj[key]; 
-  }
-  return cameled;
-};
-
-fs.readFile(dataDir + 'posts.json', function(err, data) {
-  var posts = eval('[' + data.toString().replace(/{/g, ',{').replace(',{', '{') + ']');
+fs.readFile(dataDir + 'posts2.json', function(err, data) {
+  var posts = eval(data.toString());
   db.repo(function(err, repo) {
     repo.dropDatabase(function(err, results) {
       repo.collection('posts', function(err, collection) {
         posts.forEach(function(post) {
-          collection.insert(camelize(post));
+          collection.insert(post);
         });
         db.close();
       });
