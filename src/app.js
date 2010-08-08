a98a4bcf039d67a3287e2af6a1b72aa71b444064
Brian Mavity <brian@brianmavity.com>
2010-08-07 19:30:55 -0500

    Added authentication for posts post method

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 var sys = require('sys'),
     connect = require('connect'),
+    auth = require('connect-auth'),
     app = require('express').createServer(),
     repo = require('./mongo_repository'),
     pub = __dirname + '/public';
 
-var appRoutes = {
-  root: '/',
-  post: this.root + 'posts/:slug'
-};
+var basic = Auth.Http({
+  getPasswordForUser: function(username, callback) {
+    callback(null, 'a'); 
+  },
+  useBasic: true
+});
+
+var appRoutes = (function(appRoot) {
+  var that = {};
+  that.root = '/';
+  that.post = that.root + 'posts/';
+  return that;
+})();
 
 app.set('view engine', 'jade');
 
@@ -17,6 +27,9 @@ app.use(connect.methodOverride());
 app.use(connect.compiler({ src: pub, enable: ['sass'] }));
 app.use(connect.staticProvider(pub));
 app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(connect.cookieDecoder());
+app.use(connect.session());
+app.use(Auth(basic));
 
 app.get(appRoutes.root, function(req, res) {
   repo.findAll(function(err, results) {
@@ -28,12 +41,14 @@ app.get(appRoutes.root, function(req, res) {
   });
 });
 
-app.post('/posts', function(req, res) {
-  repo.save({ author: 'Brian Mavity', postedOn: new Date() });
-  res.send('');
+app.post(appRoutes.post, function(req, res) {
+  req.authenticate(['http'], function(error, authenticated) {
+    sys.puts(sys.inspect(req));
+    res.send('');
+  });
 });
 
-app.get(appRoutes.post, function(req, res) {
+app.get(appRoutes.post + ':slug', function(req, res) {
   repo.find(req.params.slug, function(post) {
     res.render('post_index', {
       locals: {
